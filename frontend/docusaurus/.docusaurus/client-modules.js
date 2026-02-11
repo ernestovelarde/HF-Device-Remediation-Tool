6e5ff1ab986ebfb0135bb97e2fb86ec7fa7f5d46
Ernesto Velarde <ernestovelarde@Ernestos-MacBook-Pro.local>
2026-02-10 23:58:38 -0500

    Remove generated Docusaurus artifacts

--- frontend/docusaurus/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/ernestovelarde/Documents/Code/ninja-dashboard/frontend/docusaurus/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/ernestovelarde/Documents/Code/ninja-dashboard/frontend/docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/ernestovelarde/Documents/Code/ninja-dashboard/frontend/docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/ernestovelarde/Documents/Code/ninja-dashboard/frontend/docusaurus/src/css/custom.css"),
-];
